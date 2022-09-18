6683cd4ee93661011a1c2ccef49bf4413efd4424
liujiajia666li <113825103+liujiajia666li@users.noreply.github.com>
2022-09-18 17:47:35 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "",
+  APP_ID: "wxa704a8f7205a4f99",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "",
+  APP_SECRET: "228c83bd1a80c3d534379af390e648c8",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -57,24 +57,20 @@ export const config = {
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: [
+  USERS: [odsxj6lwDplED-4I4NZTm6pKGAx8]
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "小6宝贝",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "xwUusazdYUkO5N8eCvcULZCUCDRyWm56y9N9AP6wkxc",
       // 所在省份
-      province: "广东",
+      province: "天津",
       // 所在城市
-      city: "肇庆",
+      city: "天津",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
-      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
-      horoscopeDateType: '今日',
-      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn",
+      horoscopeDate: '7-10',
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
@@ -95,16 +91,16 @@ export const config = {
       ]
     },
     {
-      name: "老婆1",
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      horoscopeDate: '',
+      name: "小6宝贝",
+      id: "odsxj6szXYzj_9p6QYIh7WCl3ylQ",
+      useTemplateId: "xwUusazdYUkO5N8eCvcULZCUCDRyWm56y9N9AP6wkxc",
+      province: "天津",
+      city: "天津",
+      horoscopeDate: '7-10',
       horoscopeDateType: '',
       openUrl: "https://wangxinleo.cn",
-      festivals: [],
-      customizedDateList: []
+      festivals: [{"type": "节日", "name": "接猪纪念日", "year": "2022", "date": "08-13"},],
+      customizedDateList: [ {"keyword": "love_day", date: "2022-06-02"},]
     },
     {
       name: "老婆2",
